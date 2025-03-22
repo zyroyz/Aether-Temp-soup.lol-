c4824cedad2539248b7740581e7c6f2facc1d196
zyro <126006401+zyroyz@users.noreply.github.com>
2025-03-22 18:37:33 +0000

    Add files via upload

---
+++ AetherTemp/Classes/ExtGradient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace StupidTemplate.Classes
+{
+    public class ExtGradient
+    {
+        public GradientColorKey[] colors = new GradientColorKey[]
+        {
+            new GradientColorKey(new Color(0.627f, 0.474f, 0.587f), 1f),
+        };
+
+        public bool isRainbow = false;
+        public bool copyRigColors = false;
+    }
+}
